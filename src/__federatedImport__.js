bb8f4a1687b521dbad33eec35f5db19cdc4b95cf
MadaraUchiha-314 <rohithr31+github@gmail.com>
2023-07-27 00:55:23 -0700

    chore: comments

--- src/__federatedImport__.js
+++ src/__federatedImport__.js
@@ -1,5 +1,8 @@
 export function __federatedImport__(moduleName) {
     if (Object.prototype.hasOwnProperty.call(moduleMap, moduleName)) {
+        /**
+         * TODO: Check in the shared scope if a satisfying version already exists.
+         */
         return import(moduleMap[moduleName].chunkPath);
     }
     throw Error(`${moduleName} not available in shared scope.`);
