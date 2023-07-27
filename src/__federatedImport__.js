7611ff9708df9e14b869107967eea8a400acd600
Rohith R <6977429+MadaraUchiha-314@users.noreply.github.com>
2023-07-27 18:17:33 +0000

    feat: add semver mathcing to shared scope

--- src/__federatedImport__.js
+++ src/__federatedImport__.js
@@ -1,14 +1,22 @@
+import maxSatisfying from "semver/ranges/max-satisfying";
+
 let sharedScope = null;
 
 export function setSharedScope(inheritedSharedScope) {
     sharedScope = inheritedSharedScope;
 }
 
-export function __federatedImport__(moduleName) {
+export async function __federatedImport__(moduleName) {
     if (Object.prototype.hasOwnProperty.call(moduleMap, moduleName)) {
-        /**
-         * TODO: Check in the shared scope if a satisfying version already exists.
-         */
+        const availableVersions = Object.keys(sharedScope?.[moduleName] ?? {});
+        const { requiredVersion } = moduleMap[moduleName];
+        if (requiredVersion) {
+            const maxSatisfyingVersion = maxSatisfying(availableVersions, requiredVersion);
+            if (maxSatisfyingVersion) {
+                const module = (await moduleMap[maxSatisfyingVersion].get())();
+                return module;
+            }
+        }
         return import(moduleMap[moduleName].chunkPath);
     }
     throw Error(`${moduleName} not available in shared scope.`);
