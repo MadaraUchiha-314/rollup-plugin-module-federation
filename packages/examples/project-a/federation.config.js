a4f62ce295663a42368bbdc23f5e9d2be49b4506
MadaraUchiha-314 <rohithr31+github@gmail.com>
2023-12-03 15:28:17 -0800

    fix: url structure

--- packages/examples/project-a/federation.config.js
+++ packages/examples/project-a/federation.config.js
@@ -1,15 +1,16 @@
 const getProjectBRemoteEntry = (bundler) => {
   const remoteEntryName = 'my-remote-entry.js';
-  const packageName = 'project-b';
   if (process.env.CI && process.env.VERCEL) {
-    const domain = `${packageName}-git-${process.env.VERCEL_GIT_COMMIT_REF}-${process.env.VERCEL_GIT_REPO_SLUG}`;
+    const projectName = 'rollup-plugin-module-federation-project-b';
+    const domain = `${projectName}-git-${process.env.VERCEL_GIT_COMMIT_REF}-${process.env.VERCEL_GIT_REPO_SLUG}`;
     const url = `https://${domain}.vercel.app/${bundler}/${remoteEntryName}`;
     return url;
   }
   /**
-     * TODO: When we migrate to vite or something similar, we need to figure out the url from that.
-     */
+   * TODO: When we migrate to vite or something similar, we need to figure out the url from that.
+   */
   const domain = 'localhost:8080';
+  const packageName = 'project-b';
   const url = `http://${domain}/packages/examples/${packageName}/dist/${bundler}/${remoteEntryName}`;
   return url;
 };
