9bfbafad74782884fd2abf7435bef46c4ce0eb62
MadaraUchiha-314 <rohithr31+github@gmail.com>
2024-03-12 21:52:51 -0700

    fix: more tests

---
+++ packages/rollup-plugin-module-federation/tests/federation.test.js
@@ -0,0 +1,53 @@
+import { getFederatedImportStatementForNode } from '../src/index.ts';
+import {
+  describe, expect, test,
+} from '@jest/globals';
+
+describe('getFederatedImportStatementForNode', () => {
+  const importStmt = 'import';
+  const entityToImport = 'entity';
+
+  test('should handle ImportDeclaration with ImportDefaultSpecifier', () => {
+    const node = {
+      type: 'ImportDeclaration',
+      specifiers: [{ type: 'ImportDefaultSpecifier', local: { name: 'ABC' } }],
+    };
+    const result = getFederatedImportStatementForNode(node, { importStmt, entityToImport }, 'remote');
+    expect(result).toContain(`const ABC = (await import('entity')).default`);
+  });
+
+  test('should handle ImportDeclaration with ImportNamespaceSpecifier', () => {
+    const node = {
+      type: 'ImportDeclaration',
+      specifiers: [{ type: 'ImportNamespaceSpecifier', local: { name: 'ABC' } }],
+    };
+    const result = getFederatedImportStatementForNode(node, { importStmt, entityToImport }, 'remote');
+    expect(result).toContain(`const ABC = (await import('entity'))`);
+  });
+
+  test('should handle ImportDeclaration with ImportSpecifier', () => {
+    const node = {
+      type: 'ImportDeclaration',
+      specifiers: [{ type: 'ImportSpecifier', local: { name: 'ABC' }, imported: { name: 'XYZ' } }],
+    };
+    const result = getFederatedImportStatementForNode(node, { importStmt, entityToImport }, 'remote');
+    expect(result).toContain(`const { XYZ: ABC } = (await import('entity'))`);
+  });
+
+  test('should handle ImportExpression', () => {
+    const node = {
+      type: 'ImportExpression',
+    };
+    const result = getFederatedImportStatementForNode(node, { importStmt, entityToImport }, 'remote');
+    expect(result).toContain(`(await import('entity'))`);
+  });
+
+  test('should handle ExportNamedDeclaration', () => {
+    const node = {
+      type: 'ExportNamedDeclaration',
+      specifiers: [{ type: 'ExportSpecifier', local: { name: 'ABC' }, exported: { name: 'XYZ' } }],
+    };
+    const result = getFederatedImportStatementForNode(node, { importStmt, entityToImport }, 'remote');
+    expect(result).toContain(`const { ABC } = (await import('entity')); export { ABC as XYZ }`);
+  });
+});
