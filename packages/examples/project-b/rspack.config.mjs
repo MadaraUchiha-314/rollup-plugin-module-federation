2df26c7108c5775e1dac7de5453e40bd41a13cb6
MadaraUchiha-314 <rohithr31+github@gmail.com>
2024-03-20 21:06:26 -0700

    chore: adding rspack build to examples

---
+++ packages/examples/project-b/rspack.config.mjs
@@ -0,0 +1,40 @@
+import rspack from '@rspack/core';
+import { federationconfig } from './federation.config.js';
+
+import path from 'node:path';
+
+const __dirname = path.resolve('.');
+const { ModuleFederationPlugin } = rspack.container;
+const { CopyRspackPlugin: CopyPlugin } = rspack;
+
+const config = ({ outputFormat }) => ({
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, `dist/rspack/${outputFormat}`),
+    filename: 'index.js',
+    library: {
+      type: outputFormat === 'esm' ? 'module' : outputFormat,
+    },
+  },
+  ...(outputFormat === 'esm'
+    ? {
+        experiments: {
+          outputModule: true,
+        },
+      }
+    : {}),
+  plugins: [
+    new ModuleFederationPlugin({
+      ...federationconfig('rspack'),
+    }),
+    new CopyPlugin({
+      patterns: [{ from: `public/${outputFormat}/index.html` }],
+    }),
+  ],
+});
+
+export default [
+  config({ outputFormat: 'esm' }),
+  config({ outputFormat: 'system' }),
+];
