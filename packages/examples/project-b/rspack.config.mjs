73cc08764dd4718f1eb76f996e42bc94fe8c8f27
Rohith Ramakrishnan <6977429+MadaraUchiha-314@users.noreply.github.com>
2024-03-26 08:27:26 -0700

    chore: adding rspack build to examples #46 (#47)
    * chore: adding rspack build to examples
    
    * chore: add library type
    
    * chore: lint
    
    * chore: use rspack.future flags
    
    * chore: update to new version of rspack with bug fix

---
+++ packages/examples/project-b/rspack.config.mjs
@@ -0,0 +1,49 @@
+import rspack from '@rspack/core';
+import { federationconfig } from './federation.config.js';
+
+import path from 'node:path';
+
+const __dirname = path.resolve('.');
+const { ModuleFederationPlugin } = rspack.container;
+const { CopyRspackPlugin: CopyPlugin } = rspack;
+
+const config = ({ outputFormat }) => ({
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, `dist/rspack/${outputFormat}`),
+    filename: 'index.js',
+    library: {
+      type: outputFormat === 'esm' ? 'module' : outputFormat,
+    },
+  },
+  ...(outputFormat === 'esm'
+    ? {
+        experiments: {
+          rspackFuture: {
+            newTreeshaking: true,
+          },
+          outputModule: true,
+        },
+      }
+    : {}),
+  plugins: [
+    new ModuleFederationPlugin({
+      ...federationconfig('rspack'),
+      /**
+       * Additional stuff for webpack.
+       */
+      library: {
+        type: outputFormat === 'esm' ? 'module' : outputFormat,
+      },
+    }),
+    new CopyPlugin({
+      patterns: [{ from: `public/${outputFormat}/index.html` }],
+    }),
+  ],
+});
+
+export default [
+  config({ outputFormat: 'esm' }),
+  config({ outputFormat: 'system' }),
+];
