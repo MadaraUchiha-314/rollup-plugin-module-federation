4cf3f15d1c6320eab69cb7fc446f48e60621b559
MadaraUchiha-314 <rohithr31+github@gmail.com>
2024-06-24 09:21:39 -0700

    fix: remove local public path

--- packages/examples/project-a/rspack.config.mjs
+++ packages/examples/project-a/rspack.config.mjs
@@ -12,8 +12,6 @@ const config = async ({ outputFormat }) => ({
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, `dist/rspack/${outputFormat}`),
-    publicPath:
-      'http://localhost:8080/packages/examples/project-a/dist/rspack/esm/',
     filename: 'index.js',
     library: {
       type: outputFormat === 'esm' ? 'module' : outputFormat,
